c908e8d3d58c70a1c523092703a1dd8cd9b29df6
ruma847 <rumaambarish@gmail.com>
2025-04-25 21:46:52 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,7 @@
+string = input("Please Enter your own string : ")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Orginal String = ", string)
+print("\nThe Reversed String = ", string2)
